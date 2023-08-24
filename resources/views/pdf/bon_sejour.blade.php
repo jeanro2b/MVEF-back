27092d7dced33aeb443a9e2e700b5a68b2d0efb9
Jean-Romain Gabet <jeanromaingabet@gmail.com>
2023-08-24 11:33:24 +0200

    V4.5

--- resources/views/pdf/bon_sejour.blade.php
+++ resources/views/pdf/bon_sejour.blade.php
@@ -10,7 +10,7 @@
         body {
             margin: 0;
             padding: 0;
-            font-family: 'Nunito'; 
+            font-family: 'Nunito', 'Nunito-Regular', sans-serif; 
         }
 
         .header {
