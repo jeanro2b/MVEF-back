7fc240ba598abe2a85449a8e6ad0587145853769
Jean-Romain Gabet <jeanromaingabet@gmail.com>
2023-11-16 23:22:12 +0100

    V2.1

--- app/Http/Controllers/PlanningController.php
+++ app/Http/Controllers/PlanningController.php
@@ -12,6 +12,7 @@ use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
 
 
 class PlanningController extends Controller
