cedc490ea05e36214b57732b8547311f0dbaa502
Jean-Romain Gabet <jeanromaingabet@gmail.com>
2023-11-16 19:57:41 +0100

    V2.1

--- database/migrations/2023_11_16_132914_create_servicespayants_table.php
+++ database/migrations/2023_11_16_132914_create_servicespayants_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->foreign('destination_id')->references('id')->on('destinations');
 
             $table->string('icon')->nullable();
-            $table->text('text');
+            $table->text('text')->nullable();
         });
     }
 
