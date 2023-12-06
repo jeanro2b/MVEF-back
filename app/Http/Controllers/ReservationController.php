dbb9847f00458dcd7151fe5a257599d3ce38e569
Jean-Romain Gabet <jeanromaingabet@gmail.com>
2023-12-06 23:05:25 +0100

    V3

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Reservation;
+use App\Http\Controllers\Str;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Stripe\StripeClient;
