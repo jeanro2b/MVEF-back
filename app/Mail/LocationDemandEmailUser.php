822bedd0f1eb05b5ed200a31379fd325283f8972
Jean-Romain Gabet <jeanromaingabet@gmail.com>
2023-12-19 14:51:16 +0100

    V3

--- app/Mail/LocationDemandEmailUser.php
+++ app/Mail/LocationDemandEmailUser.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class LocationDemandEmail extends Mailable
+class LocationDemandEmailUser extends Mailable
 {
     use Queueable, SerializesModels;
 
