415deed118e35550ac0292b4f6613399d351228c
Jean-Romain Gabet <jeanromaingabet@gmail.com>
2024-01-05 19:45:15 +0100

    V3

--- app/Http/Controllers/CodeController.php
+++ app/Http/Controllers/CodeController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Code;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CodeController extends Controller
 {
