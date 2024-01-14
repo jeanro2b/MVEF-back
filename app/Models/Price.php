bf5a8810ad224ae20617d85662f158394e4bf51a
Jean-Romain Gabet <jeanromaingabet@gmail.com>
2024-01-14 16:33:19 +0100

    V4

--- app/Models/Price.php
+++ app/Models/Price.php
@@ -6,7 +6,7 @@ use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Code extends Model
+class Price extends Model
 {
     use HasFactory;
 
