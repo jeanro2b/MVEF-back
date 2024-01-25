bfa1b54092d3de72be45ad75092b1804bbf2a172
Jean-Romain Gabet <jeanromaingabet@gmail.com>
2024-01-25 17:24:11 +0100

    V4

--- app/Models/Minimum.php
+++ app/Models/Minimum.php
@@ -6,7 +6,7 @@ use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Price extends Model
+class Minimum extends Model
 {
     use HasFactory;
 
